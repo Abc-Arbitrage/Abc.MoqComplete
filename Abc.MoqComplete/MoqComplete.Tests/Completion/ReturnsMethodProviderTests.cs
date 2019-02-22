d7723607aa4cd154a34e3b1e0dc389089277e7f3
Louis Constantin <lconstan793336@gmail.com>
2019-02-22 18:05:34 +0100

    Add completion for Returns method

---
+++ Abc.MoqComplete/MoqComplete.Tests/Completion/ReturnsMethodProviderTests.cs
@@ -0,0 +1,26 @@
+﻿using JetBrains.ReSharper.FeaturesTestFramework.Completion;
+using JetBrains.ReSharper.TestFramework;
+using MoqComplete.CompletionProvider;
+using NUnit.Framework;
+
+namespace MoqComplete.Tests.Completion
+{
+    [TestNetCore21("Moq/4.10.1")]
+    public class ReturnsMethodProviderTests: CodeCompletionTestBase
+    {
+        private CallbackMethodProvider _returnsProvider;
+        protected override CodeCompletionTestType TestType => CodeCompletionTestType.Action;
+        protected override string RelativeTestDataPath => "ReturnsCompletion";
+
+        [SetUp]
+        public void SetUp()
+        {
+            // /!\ Mandatory otherwise the completion is not done
+            _returnsProvider = new CallbackMethodProvider();
+        }
+
+        [TestCase("returnsCompletion")]
+        [TestCase("returnsCompletion_afterCallback")]
+        public void should_fill_with_callback(string testSrc) => DoOneTest(testSrc);
+    }
+}
---
+++ Abc.MoqComplete/MoqComplete/CompletionProvider/ReturnsMethodProvider.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Feature.Services.CodeCompletion;
+using JetBrains.ReSharper.Feature.Services.CodeCompletion.Infrastructure;
+using JetBrains.ReSharper.Feature.Services.CodeCompletion.Infrastructure.AspectLookupItems.BaseInfrastructure;
+using JetBrains.ReSharper.Feature.Services.CodeCompletion.Infrastructure.AspectLookupItems.Info;
+using JetBrains.ReSharper.Feature.Services.CodeCompletion.Infrastructure.LookupItems;
+using JetBrains.ReSharper.Feature.Services.CSharp.CodeCompletion.Infrastructure;
+using JetBrains.ReSharper.Features.Intellisense.CodeCompletion.CSharp;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.CSharp;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+using JetBrains.ReSharper.Psi.ExpectedTypes;
+using JetBrains.ReSharper.Psi.Resources;
+using JetBrains.ReSharper.Psi.Tree;
+using MoqComplete.Extensions;
+using MoqComplete.Services;
+
+namespace MoqComplete.CompletionProvider
+{
+    [Language(typeof(CSharpLanguage))]
+    public class ReturnsMethodProvider : ItemsProviderOfSpecificContext<CSharpCodeCompletionContext>
+    {
+        protected override bool IsAvailable(CSharpCodeCompletionContext context)
+        {
+            var codeCompletionType = context.BasicContext.CodeCompletionType;
+            return codeCompletionType == CodeCompletionType.SmartCompletion || codeCompletionType == CodeCompletionType.BasicCompletion;
+        }
+
+        protected override bool AddLookupItems(CSharpCodeCompletionContext context, IItemsCollector collector)
+        {
+            var methodIdentitifer = context.BasicContext.Solution.GetComponent<IMoqMethodIdentifier>();
+            var mockedMethodProvider = context.BasicContext.Solution.GetComponent<IMockedMethodProvider>();
+            var identifier = context.TerminatedContext.TreeNode as IIdentifier;
+            var expression = identifier.GetParentSafe<IReferenceExpression>();
+
+            if (expression == null)
+                return false;
+
+            if (!(expression.ConditionalQualifier is IInvocationExpression invocation))
+                return false;
+
+            if (methodIdentitifer.IsMoqCallbackMethod(invocation))
+                invocation = invocation.InvokedExpression?.FirstChild as IInvocationExpression;
+
+            var mockedMethod = mockedMethodProvider.GetMockedMethodFromSetupMethod(invocation);
+
+            if (mockedMethod == null || mockedMethod.Parameters.Count == 0)
+                return false;
+
+            var types = mockedMethod.Parameters.Select(p => p.Type.GetPresentableName(CSharpLanguage.Instance));
+            var variablesName = mockedMethod.Parameters.Select(p => p.ShortName);
+            var proposedCallback = $"Returns<{string.Join(",", types)}>(({string.Join(",", variablesName)}) => )";
+            var item = CSharpLookupItemFactory.Instance.CreateKeywordLookupItem(context, proposedCallback, TailType.None, PsiSymbolsThemedIcons.Method.Id) as LookupItem<TextualInfo>;
+            if (item == null)
+                return false;
+
+            item.SetInsertCaretOffset(-1);
+            item.SetReplaceCaretOffset(-1);
+            item.WithInitializedRanges(context.CompletionRanges, context.BasicContext);
+            item.PlaceTop();
+            collector.Add(item);
+            return true;
+        }
+    }
+}
