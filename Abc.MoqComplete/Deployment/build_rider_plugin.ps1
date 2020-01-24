84c28d854b70f8814003d800fa7b09725a20b23a
Louis Constantin <lconstan793336@gmail.com>
2020-01-24 13:47:59 +0100

    Add version number to Directory.Build.props, change yaml to build plugins on azure devops

---
+++ Abc.MoqComplete/Deployment/build_rider_plugin.ps1
@@ -0,0 +1,48 @@
+$artifactDirectory = $args[0]
+$PluginId = "Abc.MoqComplete"
+$BuildPropsFilePath = "..\Directory.Build.props"
+
+# Get the version number
+if (Test-Path $BuildPropsFilePath) {
+	$PluginXml = [xml] (Get-Content $BuildPropsFilePath)
+	$Version = $PluginXml.SelectSingleNode(".//Project/PropertyGroup/Version").innerText
+}
+
+Write-Host "Generating Rider package"
+Write-Host "Staging directory is $artifactDirectory"
+Write-Host "Version is $Version"
+
+# Fix rider package
+Add-Type -AssemblyName System.Text.Encoding
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+class FixedEncoder : System.Text.UTF8Encoding {
+    FixedEncoder() : base($true) { }
+
+    [byte[]] GetBytes([string] $s)
+    {
+        $s = $s.Replace("\", "/");
+        return ([System.Text.UTF8Encoding]$this).GetBytes($s);
+    }
+}
+
+function Zip
+{
+    param([string]$zipfile, [string]$outpath)
+    [System.IO.Compression.ZipFile]::CreateFromDirectory($outpath, $zipfile, [System.IO.Compression.CompressionLevel]::Optimal, $True, [FixedEncoder]::new())
+}
+
+Unzip "$artifactDirectory\$PluginId.Rider.$Version.nupkg" "$artifactDirectory\$PluginId.Rider"
+rm "$artifactDirectory\$PluginId.Rider\_rels" -Force -Recurse
+rm "$artifactDirectory\$PluginId.Rider\package" -Force -Recurse
+rm "$artifactDirectory\$PluginId.Rider\*.xml"
+rm "$artifactDirectory\$PluginId.Rider\*.nuspec"
+rm "$artifactDirectory\$PluginId.Rider.$Version.nupkg"
+Zip "$artifactDirectory\$PluginId.Rider.$Version.zip" "$artifactDirectory\$PluginId.Rider"
+rm "$artifactDirectory\$PluginId.Rider" -Force -Recurse
