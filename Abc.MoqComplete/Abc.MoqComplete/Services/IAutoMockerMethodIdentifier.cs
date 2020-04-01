3d275ae66c522b651846a754821be82ea0a0011d
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2020-04-01 12:17:30 +0300

    #5 added IAutoMockerMethodIdentifier and IAutoMockerMockedMethodProvider

---
+++ Abc.MoqComplete/Abc.MoqComplete/Services/IAutoMockerMethodIdentifier.cs
@@ -0,0 +1,13 @@
+﻿using JetBrains.Annotations;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+
+namespace Abc.MoqComplete.Services
+{
+    public interface IAutoMockerMethodIdentifier
+    {
+        bool IsAutoMockerSetupMethod([CanBeNull]  IInvocationExpression invocationExpression);
+        bool IsAutoMockerVerifyMethod([CanBeNull]  IInvocationExpression invocationExpression);
+        bool IsAutoMockerReturnMethod([CanBeNull]  IInvocationExpression invocationExpression);
+        bool IsAutoMockerCallbackMethod([CanBeNull]  IInvocationExpression invocationExpression);
+    }
+}
