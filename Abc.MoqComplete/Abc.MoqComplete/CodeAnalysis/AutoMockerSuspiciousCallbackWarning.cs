d3cfa82541e477051331a3ffb85ca7ff87e28f41
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2020-04-01 12:15:47 +0300

    #5 Implement AutoMockerSuspiciousCallbackWarning

---
+++ Abc.MoqComplete/Abc.MoqComplete/CodeAnalysis/AutoMockerSuspiciousCallbackWarning.cs
@@ -0,0 +1,31 @@
+﻿using JetBrains.DocumentModel;
+using JetBrains.ReSharper.Feature.Services.Daemon;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+
+namespace Abc.MoqComplete.CodeAnalysis
+{
+	[StaticSeverityHighlighting(Severity.WARNING, HighlightingGroupIds.GutterMarksGroup)]
+	public class AutoMockerSuspiciousCallbackWarning : IHighlighting
+	{
+		private readonly DocumentRange _documentRange;
+
+		public AutoMockerSuspiciousCallbackWarning(IInvocationExpression element, DocumentRange documentRange)
+		{
+			_documentRange = documentRange;
+		}
+
+		public bool IsValid()
+		{
+			return true;
+		}
+
+		public DocumentRange CalculateRange()
+		{
+			return _documentRange;
+		}
+
+		public string ToolTip => "AutoMocker suspicious Callback method call: Generic types do not match";
+
+		public string ErrorStripeToolTip { get; }
+	}
+}
